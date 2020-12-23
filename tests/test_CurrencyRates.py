cc5e4d0ac77af2fdb467f808c9c7df91dd84ea12
Tal Shafir <tal.4000@gmail.com>
2020-12-23 15:42:35 +0200

    use constant in the tests

--- tests/test_CurrencyRates.py
+++ tests/test_CurrencyRates.py
@@ -16,21 +16,26 @@ class CurrencyRatesTestCase(unittest.TestCase):
 
     def test_convert_to_euro(self):
         _TEST_DATE = '2020-05-05'
+        _TEST_FROM_CURRENCY = 'USD'
+        _TEST_TO_CURRENCY = 'EUR'
         rates_table = CurrencyRates.get_currency_conversion_rate(ECBHandler,
-                                                                 ['USD'],
-                                                                 ['EUR'],
+                                                                 [_TEST_FROM_CURRENCY],
+                                                                 [_TEST_TO_CURRENCY],
                                                                  _TEST_DATE,
                                                                  _TEST_DATE)
-        self.assertEqual(rates_table['(USD,EUR)'][_TEST_DATE], '1.0843')
+        self.assertEqual(rates_table[f'({_TEST_FROM_CURRENCY},{_TEST_TO_CURRENCY})'][_TEST_DATE], '1.0843')
 
     def test_convert_to_not_euro(self):
         _TEST_DATE = '2020-05-05'
+        _TEST_FROM_CURRENCY = 'USD'
+        _TEST_TO_CURRENCY = 'JPY'
         rates_table = CurrencyRates.get_currency_conversion_rate(ECBHandler,
-                                                                 ['USD'],
-                                                                 ['JPY'],
+                                                                 [_TEST_FROM_CURRENCY],
+                                                                 [_TEST_TO_CURRENCY],
                                                                  _TEST_DATE,
                                                                  _TEST_DATE)
-        self.assertEqual(rates_table['(USD,JPY)'][_TEST_DATE], '0.009370840895341804')
+        self.assertEqual(rates_table[f'({_TEST_FROM_CURRENCY},{_TEST_TO_CURRENCY})'][_TEST_DATE],
+                         '0.009370840895341804')
 
 
 if __name__ == '__main__':
