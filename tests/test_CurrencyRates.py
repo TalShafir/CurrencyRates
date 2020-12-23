990f27bd500e43083968dd3012cd171c57ff3c68
Tal Shafir <tal.4000@gmail.com>
2020-12-23 15:14:54 +0200

    added tests for some currency to EUR and some currency to non-EUR currency

--- tests/test_CurrencyRates.py
+++ tests/test_CurrencyRates.py
@@ -3,7 +3,7 @@ import CurrencyRates
 import ECBHandler
 
 
-class MyTestCase(unittest.TestCase):
+class CurrencyRatesTestCase(unittest.TestCase):
     def test_convert_to_self(self):
         _TEST_DATE = '2020-05-05'
         _TEST_CURRENCY = 'USD'
@@ -13,6 +13,24 @@ class MyTestCase(unittest.TestCase):
                                                                  _TEST_DATE,
                                                                  _TEST_DATE)
         self.assertEqual(rates_table[f'({_TEST_CURRENCY},{_TEST_CURRENCY})'][_TEST_DATE], '1.0')
+
+    def test_convert_to_euro(self):
+        _TEST_DATE = '2020-05-05'
+        rates_table = CurrencyRates.get_currency_conversion_rate(ECBHandler,
+                                                                 ['USD'],
+                                                                 ['EUR'],
+                                                                 _TEST_DATE,
+                                                                 _TEST_DATE)
+        self.assertEqual(rates_table['(USD,EUR)'][_TEST_DATE], '1.0843')
+
+    def test_convert_to_not_euro(self):
+        _TEST_DATE = '2020-05-05'
+        rates_table = CurrencyRates.get_currency_conversion_rate(ECBHandler,
+                                                                 ['USD'],
+                                                                 ['JPY'],
+                                                                 _TEST_DATE,
+                                                                 _TEST_DATE)
+        self.assertEqual(rates_table['(USD,JPY)'][_TEST_DATE], '0.009370840895341804')
 
 
 if __name__ == '__main__':
